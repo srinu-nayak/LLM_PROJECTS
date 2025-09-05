bb42e7ba5932fb305bac05ca6e1a49e1abb1858a
Kent@44240 <srinunayakk7@gmail.com>
2025-09-05 15:46:31 -0400

    added prompt template

---
+++ 4.prompt_ui.py
@@ -0,0 +1,38 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate, load_prompt
+import streamlit as st
+import os
+
+
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"]
+
+model = ChatOpenAI()
+
+st.header('Reasearch Tool')
+
+
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+prompt_template_json = load_prompt("template.json")
+
+prompt = prompt_template_json.invoke(
+    {
+        "paper_input": paper_input,
+        "style_input": style_input, 
+        "length_input": length_input
+    }
+)
+
+
+if st.button('Summarize'):
+    result = model.invoke(prompt)
+    st.write(result.content)
+    
